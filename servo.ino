e9c05e71f321edda921c7cff051c16d8d4dab1e2
Dan Newcome <dan@dnuke.com>
2014-12-19 20:27:40 -0800

    wip on servo

--- servo.ino
+++ servo.ino
@@ -5,44 +5,55 @@ unsigned long lastTime = millis();
 
 void setup() {
 
+  
  Serial.begin(9600);           // set up Serial library at 9600 bps
   Serial.println("Encoder test");
   pinMode(2, INPUT_PULLUP);
   pinMode(3, INPUT_PULLUP);
+  attachInterrupt(0, doEncoder, RISING);
   
-  
-  motor.setSpeed(100);    
-  //motor.run(FORWARD);      // turn it on going forward
-  //delay(1000);
-  //motor.run(RELEASE);      // stopped
-  
+  motor.setSpeed(255);
 }
 
-int state = 0;
-int state2 = 0;
-int clicks = 0;
-int clicks2 = 0;
+volatile int state = 0;
+volatile int state2 = 0;
+volatile int clicks = 0;
+volatile int clicks2 = 0;
+volatile int dir = 0;
+volatile int pos = 0;
 
-// loop seems to run about 3ms most of the time
 void loop() {
-  if(state2 != digitalRead(3)) {
-    state2 = !state2;
-    clicks2++;
+  if(pos > 10) {
+    motor.run(FORWARD);
   }
-    
-  if(state != digitalRead(2)) {
-    state = !state;
-    clicks++;
-    if(clicks % 1000 == 0) {
-      Serial.println(clicks);
-      Serial.println(clicks2);
-      Serial.println("------");
-    }
-    if(clicks > 2000) {
-      motor.run(RELEASE);
-    }
+  else if(pos < -10) {
+    motor.run(BACKWARD);
   }
   else {
-    //Serial.println(".....");
+    motor.run(RELEASE);
   }
+}
+
+// loop seems to run about 3ms most of the time
+void doEncoder() {
+  noInterrupts();
+  
+  dir = digitalRead(3) ? 1 : -1;
+  
+  
+  clicks++;
+  pos += dir;
+  
+  // freezes interrupts if too much data
+  // sent to serial port..
+  // Serial.println(pos); 
+   
+  /*
+  if(clicks % 1000 == 0) {
+    Serial.println(clicks);
+    Serial.println(clicks2);
+    Serial.println("------");
+  }
+  */
+  interrupts();
 }
